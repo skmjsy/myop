6dd025f4b1bc71bdb7150b6bf5b04e0922274929
bmrtech <sunny@bmrtek.com>
2022-10-21 08:41:33 -0700

    update

--- selfdrive/ui/qt/onroad.cc
+++ selfdrive/ui/qt/onroad.cc
@@ -521,13 +521,6 @@ void NvgWindow::drawHud(QPainter &p) {
       p.drawPixmap(TRsign_x, TRsign_y, TRsign_w, TRsign_h, ic_trafficLight_red);
   }
 
-  auto st = lp.getStopLine();
-  auto ct = lp.getCruiseTarget();
-  QString str;
-  str.sprintf("SL: %.0f CT : %.0f", st[12], ct);
-  configFont(p, "Open Sans", 66, "Regular");
-  drawText(p, TRsign_x, 210, str, 200);
-
   int mdps_bus = car_params.getMdpsBus();
   int scc_bus = car_params.getSccBus();
 
