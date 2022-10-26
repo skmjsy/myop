52db1ce58caebfb085aec7cc23808f320b44aef0
bmrtech <sunny@bmrtek.com>
2022-10-26 08:20:33 -0700

    update

--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -401,9 +401,14 @@ class LongitudinalMpc:
 
     stopping = True if (self.stop_line and self.trafficState == 1 and not self.status and stopline_x < 100) else False
 
-    #self.stop_line_offset = interp(self.v_ego, [0, 10, 20, 25, 30], [1.0, 0.9, 0.8, 0.6, 0.4]) #10m/s = 22mph, 15m/s = 33mph 54kph, 20m/s = 45mph 72kph, 25m/s = 56mph, 30m/s = 67mph 108kph
-    #self.stop_line_offset = interp(self.v_ego, [0, 5, 15, 20, 25, 35, 40, 45, 50], [1.0, 1.10, 1.15, 1.25, 1.35, 1.45, 1.55, 1.65, 2.0]) #
-    self.stop_line_offset = interp(self.v_ego*CV.MS_TO_MPH, [0, 5, 15, 20, 25, 35, 40, 45, 50], [1.0, 1.10, 1.15, 1.25, 1.35, 1.45, 1.55, 1.65, 2.0]) #35mph-1.45, 40mph-1.55 tested.
+    #10m/s = 22mph 36kph
+    #15m/s = 33mph 54kph 
+    #20m/s = 45mph 72kph 
+    #25m/s = 56mph 90kph
+    #30m/s = 67mph 108kph
+    #self.stop_line_offset = interp(self.v_ego, [0, 10, 20, 25, 30], [1.0, 0.9, 0.8, 0.6, 0.4]) 
+    #self.stop_line_offset = interp(self.v_ego*CV.MS_TO_MPH, [0, 5, 15, 20, 25, 35, 40, 45, 50], [1.0, 1.10, 1.15, 1.25, 1.35, 1.45, 1.55, 1.65, 2.0]) #35mph-1.45, 40mph-1.55 tested.
+    self.stop_line_offset = interp(self.v_ego*CV.MS_TO_MPH, [0, 35, 40, 45], [1.0, 1.55, 1.65, 2.0]) 
 
     if stopping:
      self.on_stopping = True
@@ -411,7 +416,7 @@ class LongitudinalMpc:
      self.x_ego_obstacle_cost = ntune_scc_get("X_EGO_OBSTACLE_COST")
      self.set_weights(prev_accel_constraint)
      cruise_obstacle = np.cumsum(T_DIFFS * v_cruise_clipped) + get_safe_obstacle_distance(v_cruise_clipped, 0)
-     x_obstacles = np.column_stack([lead_0_obstacle, lead_1_obstacle, cruise_obstacle, stopline * self.stop_line_offset])
+     x_obstacles = np.column_stack([lead_0_obstacle, lead_1_obstacle, cruise_obstacle*2, stopline * self.stop_line_offset])
 
     #opkr
     #####################################################################################################################
