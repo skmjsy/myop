9c1d6a47f03a4272c348eebac51b96909220807e
bmrtech <sunny@bmrtek.com>
2022-10-30 14:15:49 -0700

    update

--- selfdrive/car/hyundai/carcontroller.py
+++ selfdrive/car/hyundai/carcontroller.py
@@ -295,23 +295,20 @@ class CarController:
             self.sm.update(0)
             
             if self.sm['longitudinalPlan'].onStop:
-            #if self.sm['longitudinalPlan'].longitudinalPlanSource == LongitudinalPlanSource.stop:
-            #if 0 < stop_distance  < 150:
-              #stop_distance = self.sm['longitudinalPlan'].stopLine[12]
+              if 0 < stop_distance  < 150:
+                stop_distance = self.sm['longitudinalPlan'].stopLine[12]
 
-              stop_distance = self.sm['longitudinalPlan'].stopLine[12]
-
-              if not CS.out.cruiseState.standstill:
-                if stop_distance < 2.0:
-                  apply_accel = self.accel - (DT_CTRL * 5.0)
-                elif aReqValue < 0.0 and stop_distance < self.stoppingdist  and apply_accel >= aReqValue:
-                  apply_accel = self.accel - (DT_CTRL * interp(CS.out.vEgo, [0.5, 2.0], [1.0, 5.0]))  
-                elif aReqValue < 0.0:
-                  stock_weight = interp(stop_distance, [6.0, 10.0, 18.0, 25.0, 32.0], [1.0, 1.0, 1.0, 1.0, 1.0])
-                  apply_accel = apply_accel * (1.0 - stock_weight) + aReqValue * stock_weight                  
-                else:
-                  stock_weight = 0.0
-                  apply_accel = apply_accel * (1.0 - stock_weight) + aReqValue * stock_weight
+                if not CS.out.cruiseState.standstill:
+                  if stop_distance < 2.0:
+                    apply_accel = self.accel - (DT_CTRL * 5.0)
+                  elif aReqValue < 0.0 and stop_distance < self.stoppingdist  and apply_accel >= aReqValue:
+                    apply_accel = self.accel - (DT_CTRL * interp(CS.out.vEgo, [0.5, 2.0], [1.0, 5.0]))  
+                  elif aReqValue < 0.0:
+                    stock_weight = interp(stop_distance, [6.0, 10.0, 18.0, 25.0, 32.0], [1.0, 1.0, 1.0, 1.0, 1.0])
+                    apply_accel = apply_accel * (1.0 - stock_weight) + aReqValue * stock_weight                  
+                  else:
+                    stock_weight = 0.0
+                    apply_accel = apply_accel * (1.0 - stock_weight) + aReqValue * stock_weight
 
               str_log2 = 'LPSource.stop: aReqValue={:02.3f} apply_accel={:02.3f}  stopLine={:03.0f} MPH={:02.0f} set_speed={:02.0f}'.format(
                           aReqValue, apply_accel, stop_distance, CS.out.vEgo*CV.MS_TO_MPH, set_speed )
