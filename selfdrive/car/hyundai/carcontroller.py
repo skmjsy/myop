bb3ba93fc44afcf78aa3662572337785c165d417
bmrtech <sunny@bmrtek.com>
2022-11-07 14:48:57 -0800

    update

--- selfdrive/car/hyundai/carcontroller.py
+++ selfdrive/car/hyundai/carcontroller.py
@@ -337,7 +337,7 @@ class CarController:
                   #apply_accel = self.accel - (DT_CTRL * interp(CS.out.vEgo*CV.MS_TO_MPH, [0.0, 10.0, 15.0, 25.0, 30.0], [0.0, 0.1, 0.15, 0.2, 0.4]))
                   #apply_accel = self.accel - (DT_CTRL * interp(CS.out.vEgo*CV.MS_TO_MPH, [0.0, 15.0, 20.0, 25.0, 30.0], [0.0, 0.1, 0.2, 0.4, 1.0]))
                   if(CS.out.vEgo*CV.MS_TO_MPH >= 15.0):
-                    apply_accel = self.accel - (DT_CTRL * interp(CS.out.vEgo*CV.MS_TO_MPH, [15.0, 20.0, 25.0, 30.0], [0.0, 0.2, 0.4, 1.0]))
+                    apply_accel = self.accel - (DT_CTRL * interp(CS.out.vEgo*CV.MS_TO_MPH, [15.0, 20.0, 25.0, 30.0], [0.0, 0.0, 0.4, 1.0]))
                   else:
                     apply_accel = apply_accel
 
