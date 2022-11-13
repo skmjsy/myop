6207f92676b57954bce709e3211408787ce3736a
bmrtech <sunny@bmrtek.com>
2022-11-13 10:58:27 -0800

    update

--- selfdrive/car/hyundai/scc_smoother.py
+++ selfdrive/car/hyundai/scc_smoother.py
@@ -220,11 +220,16 @@ class SccSmoother:
 
     # janpoo6427
     if Params().get_bool("UseNpilotManager"):
-      ascc_auto_set = ntune_option_enabled('autoCruiseSet') and enabled and (clu11_speed > 30 or CS.obj_valid) \
+      ascc_auto_set = ntune_option_enabled('autoCruiseSet') and enabled and (clu11_speed > 20) \
                   and CS.gas_pressed and CS.prev_cruiseState_speed and not CS.cruiseState_speed \
                   and (activated_hda if ntune_option_enabled('autoCruiseSetDependsOnNda') else True)
+
+      # ascc_auto_set = ntune_option_enabled('autoCruiseSet') and enabled and (clu11_speed > 30 or CS.obj_valid) \
+      #             and CS.gas_pressed and CS.prev_cruiseState_speed and not CS.cruiseState_speed \
+      #             and (activated_hda if ntune_option_enabled('autoCruiseSetDependsOnNda') else True)
+  
     else:
-      ascc_auto_set = Params().get_bool("AutoCruiseSet") and enabled and clu11_speed > 30 \
+      ascc_auto_set = Params().get_bool("AutoCruiseSet") and enabled and (clu11_speed > 20) \
                   and CS.gas_pressed and CS.prev_cruiseState_speed and not CS.cruiseState_speed \
                   and (activated_hda if Params().get_bool("AutoCruiseSetDependsOnNda") else True)      
 
