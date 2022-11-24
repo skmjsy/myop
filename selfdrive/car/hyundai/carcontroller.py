02123e7e551a2747b2c7f78b8a0651fe27edfb77
bmrtech <sunny@bmrtek.com>
2022-11-24 12:47:21 -0800

    update

--- selfdrive/car/hyundai/carcontroller.py
+++ selfdrive/car/hyundai/carcontroller.py
@@ -224,7 +224,7 @@ class CarController:
           self.e2e_standstill_timer = 0
           self.log.add('CS.clu_Vanz > 0')
         #elif self.e2e_standstill_stat and self.sm['longitudinalPlan'].e2eX[12] > 30 and self.sm['longitudinalPlan'].stopLine[12] < 10 and CS.clu_Vanz == 0:
-        elif self.e2e_standstill_stat and self.sm['longitudinalPlan'].trafficState != 2 and self.sm['longitudinalPlan'].stopLine[12] < 10 and CS.clu_Vanz == 0:
+        elif self.e2e_standstill_stat and self.sm['longitudinalPlan'].trafficState != 1 and self.sm['longitudinalPlan'].stopLine[12] < 10 and CS.clu_Vanz == 0:
           self.e2e_standstill = True
           self.e2e_standstill_stat = False
           self.e2e_standstill_timer = 0
