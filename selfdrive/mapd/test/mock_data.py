2b10f7800c3c04bf811e9b4e686054c0dcf6a136
bmrtech <sunny@bmrtek.com>
2022-11-27 21:05:07 -0800

    update

--- selfdrive/mapd/test/mock_data.py
+++ selfdrive/mapd/test/mock_data.py
@@ -3,7 +3,10 @@ from selfdrive.mapd.lib.geo import vectors, R
 from selfdrive.mapd.lib.NodesData import _MIN_NODE_DISTANCE, _ADDED_NODES_DIST, _SPLINE_EVAL_STEP, \
   _MIN_SPEED_SECTION_LENGTH, nodes_raw_data_array_for_wr, node_calculations, is_wr_a_valid_divertion_from_node, \
   spline_curvature_calculations, speed_limits_for_curvatures_data
-from scipy.interpolate import splev, splprep
+if EON:
+  from opspline import splev, splprep  # pylint: disable=E0401
+else:
+  from scipy.interpolate import splev, splprep
 import numpy as np
 import overpy
 
